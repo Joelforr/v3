3437e6cbec794824285f9f4f11e131bfdec3f752
DESKTOP\Joel <joelforr@gmail.com>
2019-09-24 04:26:02 -0400

    Revert changes made for testing

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -72,12 +72,7 @@ class Projects extends Component {
             </div>
           ))}
         </div>
-        <div>
-          <img
-            src={"src/assets/knockout/kno_thumb-01.png?nf_resize=fit&w=1000"}
-            alt="Screenshots of CSS-Tricks and CodePen homepages"
-          />
-        </div>
+
         {/* Non-featured Projects */}
         <div className="section-wrapper-a">
           <div className="projects-h1">Projects</div>
