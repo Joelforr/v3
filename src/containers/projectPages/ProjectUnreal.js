d49b4ad2ee1c47cef6d929f5340cbba9ff8fa476
DESKTOP\Joel <joelforr@gmail.com>
2020-05-26 10:37:34 -0400

    .Updated info and  layout for Pojrect Unreal page and made it slightly more responsive

--- src/containers/projectPages/ProjectUnreal.js
+++ src/containers/projectPages/ProjectUnreal.js
@@ -42,38 +42,65 @@ class ProjectUnreal extends Component {
         
         <div className="section-wrapper-a">       
           <SectionTitle>Dev Corner</SectionTitle>
-          <SectionBody>           
-            <h3>
+          <SectionBody>                       
+            <h3 style={{marginBottom:"0px"}}>
                 First and Third Person Meshes
             </h3>
-            <div>    
-                <video autoplay="true" muted="true" loop="true" style={{maxWidth: "60%", maxHeight:"100%"}}
-                    source src="https://thumbs.gfycat.com/DirtySpectacularGroundhog-mobile.mp4" type="video/mp4">
-                </video>
 
+            <div style={{display:"flex", flexWrap:"wrap"}}>
+              <p style={{maxWidth:"500px", width:"50vw", marginRight:"20px"}}>
+                Full animated meshes for both first-person and third-person view. In first-person view only the hands are visible. from a third person view full character mesh is visible and animated with aim offsets.
+              </p>
+
+              <div style={{flexBasis:"auto", flexGrow:"2"}}>    
+                <video autoplay="true" muted="true" loop="true" style={{minWidth:"100%", maxWidth: "100%", maxHeight:"100%"}}
+                    source src="https://thumbs.gfycat.com/DirtySpectacularGroundhog-mobile.mp4" type="video/mp4">    
+                </video>
+              </div>
             </div>
-            <p>
-                Full animated meshes for both first-person and third-person view. In first-person view only the hands are visible. from a third person view full character mesh is visible and animated with aim offsets
-            </p>
 
             <br/>
-            
-            <h3>
+
+            <h3 style={{marginBottom:"0px"}}>
                 Dyanmic Crosshair
             </h3>
-            <div>    
-                <video autoplay="true" muted="true" loop="true" style={{maxWidth: "60%", maxHeight:"100%"}}
+            <div style={{display:"flex", flexWrap:"wrap"}}>
+              <p style={{}}>
+              It's common in FPS games to have your shots become less accurate as you hold fire or move. While the character is moving or "spray firing" the crosshair reticle expands or blooms, visually representing the increase in spread. The two gifs are a comparison of my project and Riot Games FPS title Valorant which features a similar system.
+              </p>
+
+              <div style={{flexBasis:"auto", flexGrow:"2"}}>    
+                <video autoplay="true" muted="true" loop="true" style={{maxWidth: "90%", maxHeight:"100%"}}
                     source src="https://giant.gfycat.com/UnacceptableInnocentCrownofthornsstarfish.webm" type="video/mp4">
                 </video>
+              </div>
 
-                <video autoplay="true" muted="true" loop="true" style={{maxWidth: "90%", maxHeight:"100%"}}
+              <div style={{flexBasis:"auto", flexGrow:"2"}}>    
+                 <video autoplay="true" muted="true" loop="true" style={{maxWidth: "90%", maxHeight:"100%"}}
                     source src="https://giant.gfycat.com/HonorableSeveralIndianskimmer.webm" type="video/mp4">
                 </video> 
+              </div>
             </div>
 
-            <p>
-               It's common in FPS games to have your shots become less accurate as you hold fire or move. The lower gif shows an example of this in Valorant, RIOT Games newest FPS title. The gif above shows my C++ implementation of bullet spread or bloom with a dynamic crosshair.
-            </p>      
+            
+            <br/>
+            <h3 style={{marginBottom:"0px"}}>
+                Level Design
+            </h3>
+            <div style={{display:"flex", flexWrap:"wrap"}}>
+              <p style={{}}>
+              A prototype of a small to medium sized FPS map. Asymetircal map design for a Team Deatch Match game mode. The temple in the center of the map acts as the main point of contention, due to its prime location and high ground. There are also two side routes (short & long) that players can take to get a flank on the enemy.
+              </p>
+
+              <div style={{flexBasis:"auto", flexGrow:"2"}}>    
+                <video autoplay="true" muted="true" loop="true" style={{maxWidth: "90%", maxHeight:"100%"}}
+                    source src="https://thumbs.gfycat.com/DeafeningShorttermIndianspinyloach-mobile.mp4" type="video/mp4">
+                </video>
+              </div>
+
+            
+            </div>
+
           </SectionBody>
         </div>
 
